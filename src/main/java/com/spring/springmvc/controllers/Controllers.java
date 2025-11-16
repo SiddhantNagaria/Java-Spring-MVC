152a191f5bd335cb4fdf0f281132e4a7825421e4
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-11-16 21:21:53 +0530

    Model concept - sending data from controller to view

--- src/main/java/com/spring/springmvc/controllers/Controllers.java
+++ src/main/java/com/spring/springmvc/controllers/Controllers.java
@@ -1,18 +1,35 @@
 package com.spring.springmvc.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controllers {
 
-	
 	@GetMapping("/index")
-	public String indexJsp () {
+	public String indexJsp(Model model) {
 		System.out.println("Welcome to First JSP Page");
+		model.addAttribute("name", "Siddhant Nagaria");
+		model.addAttribute("id", 5114659);
+		
+		List<String> fr = new ArrayList<String>();
+		fr.add("rohan");
+		fr.add("rahul");
+		fr.add("rohit");
+		model.addAttribute("friends",fr);
 		return "index";
 	}
-	
+
+	@GetMapping("/about")
+	public String aboutJsp() {
+		System.out.println("About JSP Page");
+		return "about";
+	}
+
 	@GetMapping("/main")
 	public String mainHtml() {
 		return "redirect:/main.html";
--- src/main/resources/META-INF/resources/WEB-INF/jsp/index.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/index.jsp
@@ -1,5 +1,6 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,27 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>Main JSP Page</h1>
+	<h1>Main JSP Page</h1>
+	<h1>Index JSP Page</h1>
+
+	<%
+	String name = (String) request.getAttribute("name");
+	Integer id = (Integer) request.getAttribute("id");
+	List<String> friends = (List<String>) request.getAttribute("friends");
+	%>
+	<h2>
+		Name is :
+		<%=name%></h2>
+	<h2>
+		Id is :
+		<%=id%></h2>
+	<%
+	for (String s : friends) {
+	%>
+			<%= s %>
+	<%
+	}
+	%>
+
 </body>
 </html>
